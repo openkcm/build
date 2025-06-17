4e802b60be74957e59948b91e9e323632c7c53ff
Nicolae Nicora <n.nicora@sap.com>
2025-06-17 23:22:53 +0200

    fix: create VERSION file if doesn't exist

--- get-version.sh
+++ get-version.sh
@@ -8,6 +8,16 @@ if [[ -n "${VERSION_OVERRIDE:-}" ]]; then
   exit 0
 fi
 
+if [ ! -f "${PROJECT_ROOT}/VERSION" ]; then
+  if command -v git >/dev/null 2>&1; then
+    git fetch --tags --force
+    latest_tag=$(git describe --tags $(git rev-list --tags --max-count=1) 2>/dev/null || echo "v0.0.0")
+    echo "$latest_tag" > $PROJECT_ROOT/VERSION
+  else
+    echo "v0.0.0" > $PROJECT_ROOT/VERSION
+  fi
+fi
+
 VERSION="$(cat "${PROJECT_ROOT}/VERSION")"
 
 (
@@ -16,6 +26,6 @@ VERSION="$(cat "${PROJECT_ROOT}/VERSION")"
   if [[ "$VERSION" = *-dev ]] ; then
     VERSION="$VERSION-$(git rev-parse HEAD)"
   fi
-  
+
   echo "$VERSION"
 )
