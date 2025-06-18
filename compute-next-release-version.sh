26121a2a51931a7f7c7813856718b2c04ab8c899
Nicolae Nicora <n.nicora@sap.com>
2025-06-18 12:03:13 +0200

    fix: refactor the code to evaluate the NEXT_RELEASE_VERSION once only

--- compute-next-release-version.sh
+++ compute-next-release-version.sh
@@ -18,30 +18,30 @@ patch=${patch%%-*}
 
 case "$semver" in
   ("git-tags")
-      git fetch --tags
-      latest_tag=$(git describe --tags $(git rev-list --tags --max-count=1) 2>/dev/null || echo "${VERSION}")
+    git fetch --tags --force
+    latest_tag=$(git describe --tags $(git rev-list --tags --max-count=1) 2>/dev/null || echo "${VERSION}")
 
-      # Extract version components
-      major=$(echo $latest_tag | cut -d. -f1 | tr -d 'v')
-      minor=$(echo $latest_tag | cut -d. -f2)
-      patch=$(echo $latest_tag | cut -d. -f3)
+    # Extract version components
+    major=$(echo $latest_tag | cut -d. -f1 | tr -d 'v')
+    minor=$(echo $latest_tag | cut -d. -f2)
+    patch=$(echo $latest_tag | cut -d. -f3)
 
-      if jq -e '.pull_request != null' "$GITHUB_EVENT_PATH" > /dev/null; then
-         labels=$(jq -r '.pull_request.labels // [] | .[].name' "$GITHUB_EVENT_PATH")
-         if echo "$labels" | grep -q "major"; then
-           major=$((major + 1))
-           minor=0
-           patch=0
-         elif echo "$labels" | grep -q "minor"; then
-           minor=$((minor + 1))
-           patch=0
-         else
-           patch=$((patch + 1))
-         fi
-      else
-        echo "❌ Skipping release, as is not done through pull request!"
-        exit 1
-      fi
+    if jq -e '.pull_request != null' "$GITHUB_EVENT_PATH" > /dev/null; then
+       labels=$(jq -r '.pull_request.labels // [] | .[].name' "$GITHUB_EVENT_PATH")
+       if echo "$labels" | grep -q "major"; then
+         major=$((major + 1))
+         minor=0
+         patch=0
+       elif echo "$labels" | grep -q "minor"; then
+         minor=$((minor + 1))
+         patch=0
+       else
+         patch=$((patch + 1))
+       fi
+    else
+      echo "❌ Skipping release, as is not done through pull request!"
+      exit 1
+    fi
     ;;
   ("major")
     major=$((major + 1))
