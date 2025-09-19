9e575805d9656fd013b5c213d85dbc7b84a2d510
Nicolae Nicora <n.nicora@sap.com>
2025-09-19 14:41:09 +0200

    feat: use ldflags

---
+++ generate-build-metadata.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# --- Git values ---
+BRANCH=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match)
+SHA=$(git rev-parse HEAD)
+REPO_URL=$(git config --get remote.origin.url)
+ORG=$(echo "$REPO_URL" | sed -E 's#(git@|https://)([^/:]+)[:/]([^/]+)/.*#\3#')
+REPO=$(echo "$REPO_URL" | sed -E 's#.*/([^/]+)\.git#\1#')
+REPO_FULL="$ORG/$REPO"
+
+# --- Version from tags ---
+if [[ -z "${VERSION:-}" ]]; then
+  VERSION=$("$COMMON_SCRIPT_DIR/get-version.sh")
+fi
+
+# --- Build time---
+BUILD_TIME="$(date -u +"+%Y%m%d.%H%M%S")"
+
+
+# --- Final JSON ---
+echo "{\"branch\": \"refs/tags/$VERSION\",\"org\": \"$ORG\",\"product\": \"$REPO\",\"repo\": \"$REPO_FULL\",\"sha\": \"$SHA\",\"version\": \"$VERSION\",\"buildTime\": \"$BUILD_TIME\"}"
