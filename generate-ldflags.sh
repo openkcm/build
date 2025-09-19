d5de9aaa503bdf14b0e7a9cfb719e6e42d8c9663
Nicolae Nicora <n.nicora@sap.com>
2025-09-19 20:43:49 +0200

    remove unused actions

--- generate-ldflags.sh
+++ generate-ldflags.sh
@@ -18,9 +18,9 @@ fi
 BUILD_TIME="$(date -u +"+%Y%m%d.%H%M%S")"
 
 
-BUILD_META=$(cat <<EOF | base64 -w0
+ENCODED_BUILD_INFO=$(cat <<EOF | base64 -w0
 {"branch": "refs/tags/$VERSION","org": "$ORG","product": "$REPO","repo": "$REPO_FULL","sha": "$SHA","version": "$VERSION","buildTime": "$BUILD_TIME"}
 EOF
 )
 
-echo "-ldflags -X main.buildInfo=$BUILD_META"
+echo "-ldflags=-X main.buildInfo=$ENCODED_BUILD_INFO"
