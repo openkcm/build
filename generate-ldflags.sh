76888f70a5c11c9a812bfd7e64b2cbfbb2462a01
Nicolae Nicora <n.nicora@sap.com>
2025-09-20 10:41:11 +0200

    remove unused actions

--- generate-ldflags.sh
+++ generate-ldflags.sh
@@ -13,20 +13,6 @@ ORG=$(echo "$REPO_URL" | sed -E 's#(git@|https://)([^/:]+)[:/]([^/]+)/.*#\3#')
 REPO=$(echo "$REPO_URL" | sed -E 's#.*/([^/]+)\.git#\1#')
 REPO_NAME=$(basename -s .git "$REPO_URL")
 
-TAG="$VERSION"
-if git rev-parse -q --verify "refs/tags/$VERSION" >/dev/null; then
-  TAG_COMMIT_SHA=$(git rev-list -n 1 "refs/tags/$VERSION")
-elif [[ -n "${GITHUB_SHA:-}" ]]; then
-  TAG_COMMIT_SHA=$GITHUB_SHA
-else
-  TAG_COMMIT_SHA=$(git rev-parse HEAD)
-fi
-
-# Extract commit metadata from the tag commit
-COMMIT_MSG=$(git log -1 --pretty=%s "$TAG_COMMIT_SHA")
-COMMIT_AUTHOR=$(git log -1 --pretty="%an <%ae>" "$TAG_COMMIT_SHA")
-COMMIT_DATE=$(git log -1 --pretty=%cI "$TAG_COMMIT_SHA")
-
 # --- Build time ---
 BUILD_TIME="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
 
@@ -39,10 +25,7 @@ BUILD_INFO=$(jq -n \
   --arg sha "$TAG_COMMIT_SHA" \
   --arg version "$VERSION" \
   --arg buildTime "$BUILD_TIME" \
-  --arg commitMessage "$COMMIT_MSG" \
-  --arg commitAuthor "$COMMIT_AUTHOR" \
-  --arg commitDate "$COMMIT_DATE" \
-  '{branch, org, product, repo, sha, version, buildTime, commitMessage, commitAuthor, commitDate}'
+  '{branch, org, product, repo, sha, version, buildTime}'
 )
 
 ENCODED_BUILD_INFO=$(echo "$BUILD_INFO" | base64 -w0)
