ac328a94fa2027ecb957525656a1ea54c714299b
Nicolae Nicora <n.nicora@sap.com>
2025-09-21 15:14:33 +0200

    remove unused actions

--- generate-ldflags.sh
+++ generate-ldflags.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 
 # --- Git values ---
-BRANCH=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match)
 SHA=$(git rev-parse HEAD)
 REPO_URL=$(git config --get remote.origin.url)
 ORG=$(echo "$REPO_URL" | sed -E 's#(git@|https://)([^/:]+)[:/]([^/]+)/.*#\3#')
