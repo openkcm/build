53b7f9a065c4c59ca5d01c70617c6c1b94434bda
nnicora <75667951+nnicora@users.noreply.github.com>
2025-09-22 16:33:36 +0200

    feat: use ldflags for golang build (#29)

---
+++ generate-ldflags.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# --- Git values ---
+SHA=$(git rev-parse HEAD)
+REPO_URL=$(git config --get remote.origin.url)
+ORG=$(echo "$REPO_URL" | sed -E 's#(git@|https://)([^/:]+)[:/]([^/]+)/.*#\3#')
+REPO=$(echo "$REPO_URL" | sed -E 's#.*/([^/]+)\.git#\1#')
+REPO_NAME=$(basename "$REPO_URL")
+
+# --- Version from tags ---
+if [[ -z "${VERSION:-}" ]]; then
+  VERSION=$("$COMMON_SCRIPT_DIR/get-version.sh")
+fi
+
+# --- Build time ---
+BUILD_TIME="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+
+# --- Build JSON ---
+ENCODED_BUILD_INFO=$(cat <<EOF | base64 -w0
+{"branch": "refs/tags/$VERSION","org": "$ORG","product": "$REPO_NAME","repo": "$REPO","sha": "$SHA","version": "$VERSION","buildTime": "$BUILD_TIME"}
+EOF
+)
+
+# Output for -ldflags
+echo "-X main.BuildInfo=base64($ENCODED_BUILD_INFO)"
